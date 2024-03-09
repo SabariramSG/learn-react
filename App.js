fe0aee949785e789c6671f0ac0ef0cbf6ba04923
sg.sabariram <sg.sabariram@servicenow.com>
2024-03-10 00:41:18 +0530

    Basics of react - React elements using createElement() - React functional components - React compositions

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Creating react element using createElement() method
+
 const heading1 = React.createElement(
   "h1",
   {
@@ -22,7 +27,31 @@ const container = React.createElement(
   [heading1, heading2]
 );
 
+// Creating react element using JSX
+
+const jsxReactElement = <h1>Hey I am a JSX react element</h1>;
+
+// React functional components
+
+const Title = () => {
+  return (
+    <div>
+      <h1>{"Hey I am React functional component"}</h1>
+
+      <h2>
+        {"I can include react objects into myself as well"}
+        {heading1}
+      </h2>
+    </div>
+  );
+};
+
 // What is the difference between React and ReactDOM
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+// root.render(container);
+
+// root.render(jsxReactElement);
+
+root.render(<Title></Title>);
